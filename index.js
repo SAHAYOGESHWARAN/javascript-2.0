a806fa531aa83090f2f1d4927b4a47664c0898fb
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-07-21 07:51:38 +0530

    add javascript2.0 master

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+
+window.onload = function () {
+    const elementmain = document.getElementById("main");
+   elementmain.style.color ="red";
+  
+
+   const button = document.querySelector("button")
+   
+
+};
