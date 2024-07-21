5c407b24ee835e51263becadf1749007269d1184
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-07-21 08:14:01 +0530

    add javascript2.0 master

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-
-
-
 window.onload = function () {
     const elementmain = document.getElementById("main");
-   elementmain.style.color ="red";
+    elementmain.style.color = "red";
   
-
-   const button = document.querySelector("button")
+    const clickHandle = e => {
+        console.log(e.target.innerHTML);
+    };
    
+    const buttons = document.querySelectorAll("button");
 
+    buttons.forEach(eachButton => {
+        eachButton.addEventListener("click", clickHandle);
+    });
+
+    setTimeout(() => {
+        console.log('removed');
+        buttons.forEach(eachButton => {
+            eachButton.removeEventListener("click", clickHandle);
+        });
+    }, 10000);
 };
